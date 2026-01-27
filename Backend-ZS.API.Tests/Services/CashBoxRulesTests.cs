fe0502559df8ef71f5c8cffde8e1439747fff772
matiassromo <matiasromo10@hotmail.com>
2026-01-27 14:59:51 -0500

    unit test implementation

---
+++ Backend-ZS.API.Tests/Services/CashBoxRulesTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Backend_ZS.API.Tests.Services
+{
+    public static class CashBoxRules
+    {
+        public static decimal CalculateClosingBalance(decimal opening, decimal payments)
+        {
+            if (opening < 0 || payments < 0)
+                throw new ArgumentOutOfRangeException();
+
+            return opening + payments;
+        }
+    }
+
+    public class CashBoxRulesTests
+    {
+        [Fact]
+        public void CalculateClosingBalance_ReturnsCorrectSum()
+        {
+            // Arrange
+            var opening = 100m;
+            var payments = 50m;
+
+            // Act
+            var result = CashBoxRules.CalculateClosingBalance(opening, payments);
+
+            // Assert
+            result.Should().Be(150m);
+        }
+
+        [Fact]
+        public void CalculateClosingBalance_WhenNegative_Throws()
+        {
+            var act = () => CashBoxRules.CalculateClosingBalance(-1, 10);
+
+            act.Should().Throw<ArgumentOutOfRangeException>();
+        }
+    }
+}
